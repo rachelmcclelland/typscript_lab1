c9d8333d83f95f939ca09aeec4120c4f03c03ecc
rachelmcclelland <rachelmcclelland97@hotmail.com>
2018-01-24 15:32:36 +0000

    Part 3

---
+++ lab1_p3.js
@@ -0,0 +1,7 @@
+function numOfChars(value) {
+    count = value.length;
+    return count;
+}
+var count;
+var value = "test 1";
+console.log("Number of characters in the string (including spaces) is: " + numOfChars(value));
---
+++ lab1_p3.ts
@@ -0,0 +1,11 @@
+function numOfChars(value) {
+
+    count = value.length;
+
+    return count;
+}
+
+let count:number;
+let value:String = "test 1";
+
+console.log("Number of characters in the string (including spaces) is: " + numOfChars(value))
